2e364cbc55396d5f750e8febcb257b8c8cfb9bae
Marc Rousseau <marcetiennerousseau@gmail.com>
2023-03-28 15:10:07 +0200

    Website framework

---
+++ app/run.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import os, sys
+import logging
+FORMAT = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s"
+logging.basicConfig(format=FORMAT)
+
+
+def create_app(config_name=None):
+
+    # Initialize flask app
+    app = Flask(__name__)
+    logging.warning(f"--> Creating Server App with config: '{config_name}'")
+
+    if app.config['DEBUG']:
+        logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+    FORMAT = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s"
+    logging.basicConfig(format=FORMAT)
+
+    # Registering all the blueprints
+    from app.site_routes import site
+    app.register_blueprint(site)
+
+    return app
+
+
+# Launch the web server
+if __name__ == '__main__': # pragma: no cover
+    app = create_app()
+    debug = app.config["DEBUG"]
+    app.run(debug=debug, host="0.0.0.0", port=int(os.environ.get("PORT", 8087)))
---
+++ app/wsgi.py
@@ -0,0 +1,3 @@
+from app.run import *
+
+app = create_app()
