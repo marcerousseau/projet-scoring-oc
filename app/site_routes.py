88361c2bf6db07091de6b9fb96b79debbd2aa4d0
Marc Rousseau <marcetiennerousseau@gmail.com>
2023-03-28 16:08:52 +0200

    Solving app import error

--- app/site_routes.py
+++ app/site_routes.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+import pickle   
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 site = Blueprint('site', __name__)
 
 @site.route('/')
 def index():
     return render_template('landing.html')
+
+@site.route('/api/<customer_id>')
+def api(customer_id):
+    model = pickle.load(open('best_model.pkl', 'rb'))
+    df = pd.read_csv('df.csv')
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.fillna(df.median(), inplace=True)
+    X = df.drop(columns=["TARGET"])
+    scaler = StandardScaler()
+    X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+    pickle.dump(X, open('X.pkl', 'wb'))
+    prediction = model.predict(X[X['SK_ID_CURR']==customer_id])
+    return jsonify({'customer_id': customer_id, 'prediction': prediction[0]})
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,3 +1,3 @@
-from app.run import *
+from run import *
 
 app = create_app()
